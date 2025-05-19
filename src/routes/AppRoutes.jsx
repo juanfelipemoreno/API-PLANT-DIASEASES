4052b04057b4c6bdf39c055afda358392c9df560
pipeCode <felipe.moreno23@hotmail.es>
2025-05-19 18:29:35 -0500

    Rutas actualizada

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { useContext, React} from 'react';
 import { BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 import Home from '../Pages/Home';
+import Products from '../Pages/Products';
 
 
 export default function AppRoutes() {
@@ -9,6 +10,7 @@ export default function AppRoutes() {
             <Routes>
                 <Route path="/" element={<Home/>} />
                 <Route path="/Home" element={<Home/>}  />
+                <Route path="/Products" element={<Products/>}  />
                 <Route path="*" element={<div><h1>404 NOT FOUND</h1></div>} />
             </Routes>
         </BrowserRouter>
